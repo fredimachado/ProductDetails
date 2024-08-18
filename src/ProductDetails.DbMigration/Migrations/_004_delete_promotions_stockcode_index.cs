6ebff0476c56ceb7c7072e3ab45780e1f663f791
Fredi Machado <fredisoft@gmail.com>
2024-08-18 17:37:53 +1000

    Remove stockcode index on promotions collection
    This will allow persisting multiple promotions for the same product

---
+++ src/ProductDetails.DbMigration/Migrations/_004_delete_promotions_stockcode_index.cs
@@ -0,0 +1,12 @@
+﻿using ProductDetails.Infrastructure.Data.Promotions;
+
+namespace ProductDetails.DbMigration.Migrations;
+
+public class _004_delete_promotions_stockcode_index : IMigration
+{
+    public async Task UpgradeAsync()
+    {
+        await DB.Index<PromotionEntity>()
+            .DropAsync("Stockcode(Asc)");
+    }
+}
